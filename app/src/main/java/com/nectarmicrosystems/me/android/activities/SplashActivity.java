c6edc93ab08bab6bb6efd6efc3175e0e6c5acbd4
Tobi Adeyinka <tobex9434@yahoo.com>
2017-06-06 20:47:45 +0200

    setup MainActivity DashFragment

--- app/src/main/java/com/nectarmicrosystems/me/android/activities/SplashActivity.java
+++ app/src/main/java/com/nectarmicrosystems/me/android/activities/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.nectarmicrosystems.me.android.activities;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -8,6 +9,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
--- app/src/main/java/com/nectarmicrosystems/me/android/fragments/main_activity/DashFragment.java
+++ app/src/main/java/com/nectarmicrosystems/me/android/fragments/main_activity/DashFragment.java
@@ -1,10 +1,25 @@
 package com.nectarmicrosystems.me.android.fragments.main_activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
 import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+
+import com.nectarmicrosystems.me.R;
 
 /**
  * Created by tobi on 2017. 06. 06..
  */
 
 public class DashFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_dash_main_activity, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
 }
