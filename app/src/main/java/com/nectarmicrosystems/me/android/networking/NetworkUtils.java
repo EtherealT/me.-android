a4ca38c2b54802cf35be9196a53c9b1f35ee12d1
Tobi Adeyinka <tobex9434@yahoo.com>
2017-06-07 04:45:03 +0200

    implementing weather api

--- app/src/main/java/com/nectarmicrosystems/me/android/networking/NetworkUtils.java
+++ app/src/main/java/com/nectarmicrosystems/me/android/networking/NetworkUtils.java
@@ -1,5 +1,7 @@
 package com.nectarmicrosystems.me.android.networking;
 
+import android.util.Log;
+
 import java.net.URL;
 import java.util.Scanner;
 import java.io.IOException;
@@ -13,20 +15,17 @@ import java.net.HttpURLConnection;
 public class NetworkUtils {
 
     public static String query(URL url) throws IOException {
+        Log.i(NetworkUtils.class.getCanonicalName(), "querying " + url);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
         try {
             InputStream in = urlConnection.getInputStream();
-
             Scanner scanner = new Scanner(in);
             scanner.useDelimiter("\\A");
 
             boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
-            }
+            if (hasInput) return scanner.next();
+            else return null;
         } finally {
             urlConnection.disconnect();
         }
--- app/src/main/java/com/nectarmicrosystems/me/android/networking/open_weather_map/WeatherQueries.java
+++ app/src/main/java/com/nectarmicrosystems/me/android/networking/open_weather_map/WeatherQueries.java
@@ -17,18 +17,32 @@ class WeatherQueries {
     private static final String BASE_URL = "api.openweathermap.org/data/2.5/weather?q=";
     private static final String API_KEY = Sensitive.OPEN_WEATHER_MAP_API_KEY;
 
+    /**
+     * get the current weather data for a city using the city name and country ISO code
+     *
+     * @param city Name of the city
+     * @param countryISO3166 ISO3166 Country code
+     * @return String JSON representation of current weather data
+     * @throws IOException
+     */
     public static String getCurrentWeather(String city, String countryISO3166) throws IOException {
         return getCurrentWeather(city + "," + countryISO3166);
     }
 
+    /**
+     * get the current weather data for a city using the city name
+     *
+     * @param city Name of the city
+     * @return String JSON representation of current weather data
+     * @throws IOException
+     */
     public static String getCurrentWeather(String city) throws IOException {
         try {
-            URL url = new URL(BASE_URL + city);
+            URL url = new URL(BASE_URL + city + "&appid=" + API_KEY);
             return query(url);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 
