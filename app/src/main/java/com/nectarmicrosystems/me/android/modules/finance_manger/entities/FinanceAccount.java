a6532cb3ac120792eff392d23e314555bdefadd4
Tobi Adeyinka <tobex9434@yahoo.com>
2017-08-18 03:04:27 +0200

    finance account model impl

--- app/src/main/java/com/nectarmicrosystems/me/android/modules/finance_manger/entities/FinanceAccount.java
+++ app/src/main/java/com/nectarmicrosystems/me/android/modules/finance_manger/entities/FinanceAccount.java
@@ -1,8 +1,81 @@
 package com.nectarmicrosystems.me.android.modules.finance_manger.entities;
 
+import java.util.UUID;
+import java.math.BigDecimal;
+
 /**
  * Created by Tobi Adeyinka on 2017. 08. 18..
  */
 
 public class FinanceAccount {
+
+    /*
+     * unique auto generated id of an account
+     */
+    private final UUID id;
+
+    private String name;
+    private BigDecimal balance;
+    private FinanceAccountType accountType;
+
+    public FinanceAccount(String name, FinanceAccountType accountType, BigDecimal balance) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public FinanceAccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(FinanceAccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    /*
+     * remove the amount from the balance and return the new balance
+     */
+    public BigDecimal withdraw(BigDecimal amount){
+        balance = balance.subtract(amount);
+        return balance;
+    }
+
+    /*
+     * add the amount to the balance and return the new balance
+     */
+    public BigDecimal deposit(BigDecimal amount){
+        balance = balance.add(amount);
+        return balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FinanceAccount that = (FinanceAccount) o;
+        return id.equals(that.id)
+                && name.equals(that.name)
+                && balance.equals(that.balance)
+                && accountType == that.accountType;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + balance.hashCode();
+        result = 31 * result + accountType.hashCode();
+        return result;
+    }
+
 }
---
+++ app/src/main/java/com/nectarmicrosystems/me/android/modules/finance_manger/entities/FinanceAccountType.java
@@ -0,0 +1,10 @@
+package com.nectarmicrosystems.me.android.modules.finance_manger.entities;
+
+/**
+ * Created by Tobi Adeyinka on 2017. 08. 18..
+ */
+
+public enum FinanceAccountType {
+    CURRENT,
+    SAVINGS
+}
