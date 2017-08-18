24bb628e6e3cabfd6e10acb05ae2202b535344f5
Tobi Adeyinka <tobex9434@yahoo.com>
2017-08-18 14:59:51 +0200

    dev progress

---
+++ app/src/androidTest/java/com/nectarmicrosystems/me/android/database/repositories/FinanceAccountsRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.nectarmicrosystems.me.android.database.repositories;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import com.nectarmicrosystems.me.android.TestUtils;
+import com.nectarmicrosystems.me.android.modules.finance_manger.entities.FinanceAccount;
+import com.nectarmicrosystems.me.android.modules.finance_manger.entities.FinanceAccountType;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.logging.Logger;
+
+/**
+ * Created by Tobi Adeyinka on 2017. 08. 18..
+ */
+
+public class FinanceAccountsRepositoryTest {
+
+    private static final Logger LOGGER = Logger.getLogger(FinanceAccountsRepositoryTest.class.getCanonicalName());
+
+    private Context context;
+    private FinanceAccountsRepository financeAccountsRepository;
+
+    @Before
+    public void setup() {
+        TestUtils.setupLogger(LOGGER);
+        context = InstrumentationRegistry.getTargetContext();
+        financeAccountsRepository = new FinanceAccountsRepository(context);
+    }
+
+    @Test
+    public void testInsertNewFinanceAccount(){
+        FinanceAccount account = createFinanceAccountForTest();
+        financeAccountsRepository.insert(account);
+    }
+
+    private FinanceAccount createFinanceAccountForTest(){
+        return new FinanceAccount("Test Account", new BigDecimal(2000), FinanceAccountType.SAVINGS);
+    }
+
+}
